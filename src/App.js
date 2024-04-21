4e6c2fa48961ccce5ce69bbf25ac99d0129583a0
Marco de Castro <castromar189@gmail.com>
2024-04-21 04:43:19 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Cadastro from './pages/Cadastro.js';
+import Login from './pages/Login.js';
+import { AuthProvider } from './contexts/AuthContext.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <BrowserRouter>
+  <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </AuthProvider>
+  </BrowserRouter>
+
+);
 
 export default App;
+
